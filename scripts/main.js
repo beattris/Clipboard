cbc13e7cc72451a4e58c8567a42c85474ae1dc38
Beattris <beatriceebirim@gmail.com>
2020-08-24 08:48:08 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+let message = "Clicking this button immediately begins the iOS download";
+let message2 = "Clicking this button immediately begins the Mac download"
---
+++ scripts/utils.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("ios");
+button.addEventListener("click", function(){
+    return message;
+});
+
+const button2 = document.getElementById("mac");
+button2.addEventListener("click", function(){
+    return message2;
+});
